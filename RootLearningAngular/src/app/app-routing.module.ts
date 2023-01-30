94e96535473ee7278f0607bc12d2f9f72f4740da
Samuel Arnaiz de la Rosa <samuel.arnaiz@campusfp.es>
2023-01-30 14:47:28 +0100

    Enrutadas todas las páginas

--- RootLearningAngular/src/app/app-routing.module.ts
+++ RootLearningAngular/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from "./paginas/home/home.component";
+import { BusquedaComponent} from "./paginas/busqueda/busqueda.component";
+import { ProductoComponent} from "./paginas/producto/producto.component";
+import {LoginComponent} from "./paginas/login/login.component";
+import { RegisterComponent} from "./paginas/register/register.component";
+import { CestaComponent} from "./paginas/cesta/cesta.component";
 
 const routes: Routes = [
+  {path: "",component:HomeComponent},
+  {path:"busqueda",component:BusquedaComponent},
+  {path: "producto", component:ProductoComponent},
+  {path: "login", component:LoginComponent},
+  {path: "regitro", component:RegisterComponent},
+  {path:"cesta",component:CestaComponent}
 ];
 
 @NgModule({
