4e59abce72fa292a139db61eb74d68e17d043eaa
Samuel Arnaiz de la Rosa <samuel.arnaiz@campusfp.es>
2023-01-30 14:11:00 +0100

    creación de componente cuInfoCurso

--- RootLearningAngular/src/app/app.module.ts
+++ RootLearningAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CuInfoPrincipalComponent } from './paginas/curso/cu-info-principal/cu-info-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuInfoPrincipalComponent
   ],
   imports: [
     BrowserModule,
