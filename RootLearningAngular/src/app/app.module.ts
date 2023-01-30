7b208ad8ca0fd575aa19e40d294729b53e4b75d3
SamuelArnaizdelaRosa <samuel.arnaiz@campusfp.es>
2023-01-30 18:01:07 +0100

    Implementado caracteristicas curso y compra en producto.component

--- RootLearningAngular/src/app/app.module.ts
+++ RootLearningAngular/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { RegisterComponent } from './paginas/register/register.component';
 import { CestaComponent } from './paginas/cesta/cesta.component';
 import { BBuscadorComponent } from './componentes/busqueda/b-buscador/b-buscador.component';
 import { PCabeceraComponent } from './componentes/producto/p-cabecera/p-cabecera.component';
+import { PCaractCursoComponent } from './componentes/producto/p-caract-curso/p-caract-curso.component';
+import { PCompraComponent } from './componentes/producto/p-compra/p-compra.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { PCabeceraComponent } from './componentes/producto/p-cabecera/p-cabecera
     RegisterComponent,
     CestaComponent,
     BBuscadorComponent,
-    PCabeceraComponent
+    PCabeceraComponent,
+    PCaractCursoComponent,
+    PCompraComponent
   ],
   imports: [
     BrowserModule,
---
+++ RootLearningAngular/src/app/componentes/producto/p-compra/p-compra.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-p-compra',
+  templateUrl: './p-compra.component.html',
+  styleUrls: ['./p-compra.component.css']
+})
+export class PCompraComponent {
+
+}
