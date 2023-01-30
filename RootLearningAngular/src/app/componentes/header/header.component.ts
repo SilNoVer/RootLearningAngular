87ca441adf758bd363c462d18cfbba4ad8880262
RubenAlvarezCampus <ruben.alvarez@campusfp.es>
2023-01-30 17:29:48 +0100

    Header Implementado

--- RootLearningAngular/src/app/componentes/header/header.component.ts
+++ RootLearningAngular/src/app/componentes/header/header.component.ts
@@ -5,6 +5,48 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  constructor () {
+
+  }
+
+  showBar() {
+    let elem = document.getElementById("texto_busqueda");
+    let buscar = document.getElementById("barra_busqueda");
+    let boton_salir = document.getElementById("boton_salida");
+    let boton_buscar = document.getElementById("boton_busqueda");
+
+    if (elem.className=="activo") {
+        elem.className = "desactivado";
+        buscar.className = "busqueda";
+        boton_salir.className = "boton_salida--desactivado";
+        boton_buscar.className = "boton_busqueda--active";
+    }
+    else {
+        elem.className = "activo";
+        buscar.className = "busqueda--active";
+        boton_salir.className = "boton_salida--active";
+        boton_buscar.className = "boton_busqueda--desactivado";
+    }
+
+  }
+
+  showBurger() {
+    let elem1 = document.getElementById("elementos_links");
+    if (elem1.className=="menu_cerrado") {
+        elem1.className = "menu_abierto";
+    }
+    else {
+        elem1.className = "menu_cerrado";
+    }
+  }
+
+  loaded() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const busqueda = urlParams.get('busqueda')
+
+    document.getElementById("busqueda_texto_item").innerHTML = busqueda
+  }
 }
